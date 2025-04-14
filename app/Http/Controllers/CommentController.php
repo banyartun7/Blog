4dbcd99afb6abd5a059900d1904ca6148bfccf58
Genosbanyar <bnyar012@gmail.com>
2025-04-14 20:56:54 +0630

    mail system

--- app/Http/Controllers/CommentController.php
+++ app/Http/Controllers/CommentController.php
@@ -4,7 +4,9 @@ namespace App\Http\Controllers;
 
 use App\Models\Blog;
 use App\Models\Comment;
+use App\Mail\SubscriberMail;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Mail;
 
 class CommentController extends Controller
 {
@@ -12,11 +14,17 @@ class CommentController extends Controller
         $commentBody = request()->validate([
             'comment' => 'required'
         ]);
+
         $blog->comments()->create([
             'body' => $commentBody['comment'],
             'user_id' => auth()->id(),
             'blog_id' => $blog->id
         ]);
+
+        $subscribers = $blog->subscribers->filter(fn($subscriber)=>$subscriber->id !== auth()->id());
+        $subscribers->each(function($subscriber) use ($blog){
+            Mail::to($subscriber->email)->send(new SubscriberMail($blog));
+        });
         return redirect('blogs/'.$blog->slug)->with('status', config('alert.comment.create'));
     }
 }
